e37c72d6f0bc62f7e688aab96aa49d74947f43e5
Benedict - Ampea-Badu <104174272+BennyBlvck@users.noreply.github.com>
2023-03-23 15:07:33 +0000

    Update App.js

--- smart-brain/src/App.js
+++ smart-brain/src/App.js
@@ -74,7 +74,7 @@ class App extends Component {
     .then (response => response.json())
       .then(response => {
         if (response) {
-          fetch ('https://chipper-flan-5944ec.netlify.app:3000/image', {
+          fetch ('https://chipper-flan-5944ec.netlify.app/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
