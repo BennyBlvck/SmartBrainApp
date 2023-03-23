a3b3ed5bfdcf0e98ae7c02d85f56571665f767df
Benedict - Ampea-Badu <104174272+BennyBlvck@users.noreply.github.com>
2023-03-23 13:16:08 +0000

    Update server.js

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -28,7 +28,7 @@ app.use(cors());
 
 
 app.get('/', (req, res) => {
-    res.send(database.users)
+    res.send('It is Working')
 })
 
 app.post('/signin', (req, res) => { handleSignin(req, res, db, bcrypt) })
