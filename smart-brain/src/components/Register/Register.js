0787d4915e1580543dc121c0c215162cf0a2f23e
Benedict - Ampea-Badu <104174272+BennyBlvck@users.noreply.github.com>
2023-03-23 15:19:35 +0000

    Update Register.js

--- smart-brain/src/components/Register/Register.js
+++ smart-brain/src/components/Register/Register.js
@@ -24,7 +24,7 @@ class Register extends React.Component {
         }
 
         onSubmitSignIn = () => {
-            fetch ('https://mnrwfwvbxkzbbsznyfag.supabase.co/register', {
+            fetch ('https://chipper-flan-5944ec.netlify.app/register', {
                 method: 'post',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify({
