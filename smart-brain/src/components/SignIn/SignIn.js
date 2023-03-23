489448f3f67d1cb8e4ac15620c683423f04ffa44
Benedict - Ampea-Badu <104174272+BennyBlvck@users.noreply.github.com>
2023-03-23 13:21:08 +0000

    Update SignIn.js

--- smart-brain/src/components/SignIn/SignIn.js
+++ smart-brain/src/components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://chipper-flan-5944ec.netlify.app/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
