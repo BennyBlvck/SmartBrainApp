80a55a3ffc776a0b637681d9700e5a6fe9589481
BennyBlvck <kmenana@gmail.com>
2023-03-26 05:10:04 +0100

    adding connection to database

--- smart-brain/src/components/Register/Register.js
+++ smart-brain/src/components/Register/Register.js
@@ -1,5 +1,5 @@
 import React from "react";
-import authFetch from "../../../netlify/functions/authFetch/authFetch";
+import authFetch from "../../netlify/functions/authFetch/authFetch";
 
 class Register extends React.Component {
   constructor(props) {
