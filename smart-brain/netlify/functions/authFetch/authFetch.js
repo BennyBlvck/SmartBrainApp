e3f271b34477d885da98a805a56eebe05ee00ca3
BennyBlvck <kmenana@gmail.com>
2023-03-26 05:22:39 +0100

    adding connection to database

--- smart-brain/netlify/functions/authFetch/authFetch.js
+++ smart-brain/netlify/functions/authFetch/authFetch.js
@@ -1,31 +1,31 @@
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
 
-const authFetch = async (url, method = 'GET', data) => {
-  const user = JSON.parse(localStorage.getItem('user'));
+// const authFetch = async (url, method = 'GET', data) => {
+//   const user = JSON.parse(localStorage.getItem('user'));
 
-  if (user) {
-    try {
-      const response = await fetch(url, {
-        method,
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${user.token}`
-        },
-        body: JSON.stringify(data)
-      });
+//   if (user) {
+//     try {
+//       const response = await fetch(url, {
+//         method,
+//         headers: {
+//           'Content-Type': 'application/json',
+//           'Authorization': `Bearer ${user.token}`
+//         },
+//         body: JSON.stringify(data)
+//       });
 
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
+//       if (!response.ok) {
+//         throw new Error(response.statusText);
+//       }
 
-      return response.json();
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
-  } else {
-    throw new Error('User not authenticated');
-  }
-};
+//       return response.json();
+//     } catch (error) {
+//       console.log(error);
+//       throw error;
+//     }
+//   } else {
+//     throw new Error('User not authenticated');
+//   }
+// };
 
-export default authFetch
+// export default authFetch
