e03e2aab01e6d4ced594c535c352e25cfd23e5bf
BennyBlvck <kmenana@gmail.com>
2023-03-26 04:53:52 +0100

    adding connection to database

---
+++ smart-brain/netlify/functions/authFetch/authFetch.js
@@ -0,0 +1,33 @@
+// for a full working demo of Netlify Identity + Functions, see https://netlify-gotrue-in-react.netlify.com/
+
+import fetch from 'node-fetch';
+
+const authFetch = async (url, method = 'GET', data) => {
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  if (user) {
+    try {
+      const response = await fetch(url, {
+        method,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${user.token}`
+        },
+        body: JSON.stringify(data)
+      });
+
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+
+      return response.json();
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  } else {
+    throw new Error('User not authenticated');
+  }
+};
+
+export default authFetch;
