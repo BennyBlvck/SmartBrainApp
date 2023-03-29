8919425759fb38a0974219aa2b4fc4ad066cb146
BennyBlvck <kmenana@gmail.com>
2023-03-29 23:58:54 +0100

    adding connection to database

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -1,67 +1,100 @@
-import express from "express";
-import bcrypt from "bcrypt-nodejs";
-import cors from "cors";
-import knex from 'knex'; 
-import handleRegister from "./controllers/register.js";
-import handleSignin from "./controllers/signin.js";
-import handleProfileGet from "./controllers/profile.js";
-// import handleImage from "./controllers/image.js";
-import funcs from "./controllers/image.js";
-import { createClient } from '@supabase/supabase-js'
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = 'https://mnrwfwvbxkzbbsznyfag.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-const db = knex({
-    client: 'postgres',
-  connection: {
-    host: 'https://mnrwfwvbxkzbbsznyfag.supabase.co',
-    port: 5432,
-    user: 'Postgres',
-    password: 'DundeeBlvckz93#',
-    database: 'SmartBrain',
-    ssl: { rejectUnauthorized: false },
-  },
-  pool: {
-    min: 2,
-    max: 10,
-  },
-})
-
+const supabase = createClient('https://mnrwfwvbxkzbbsznyfag.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ucndmd3ZieGt6YmJzem55ZmFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzk1Nzk1MzgsImV4cCI6MTk5NTE1NTUzOH0.UEEcYkD-8kfozJoqZBtAWvQs1Mmm8jfJpzhvxHl3AXY');
 
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
 
+const database = knex({
+    client: 'pg',
+    connection: {
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
+    }
+});
 
 app.get('/', (req, res) => {
-    res.send('It is Working')
-})
-
-app.post('/api/signin', (req, res) => { handleSignin(req, res, db, bcrypt) })
-
-app.post('/api/register', (req, res) => { handleRegister(req, res, db, bcrypt) })
-
-app.get('/profile/:id', (req, res) => { handleProfileGet(req, res, db) })
-
-app.put('/image', (req, res) => { funcs.handleImage(req, res, db) });
-
-app.post('/imageurl', (req, res) => { funcs.handleApiCall(req, res) });
-
-app.post('/.netlify/functions/server/signin', (req, res) => {
-    handleSignin(req, res, db, bcrypt)
-    // Handle Netlify API signin logic
+    res.send('Server is working!');
 });
 
-app.post('/.netlify/functions/server/register', (req, res) => {
-    handleRegister(req, res, db, bcrypt)
-    // Handle Netlify API register logic
+app.post('/signin', (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json('Incorrect form submission');
+    }
+    database.select('email', 'hash').from('login')
+        .where('email', '=', email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(password, data[0].hash);
+            if (isValid) {
+                return database.select('*').from('users')
+                    .where('email', '=', email)
+                    .then(user => {
+                        res.json(user[0]);
+                    })
+                    .catch(err => res.status(400).json('Unable to get user'));
+            } else {
+                res.status(400).json('Wrong credentials');
+            }
+        })
+        .catch(err => res.status(400).json('Wrong credentials'));
 });
 
+app.post('/register', (req, res) => {
+    const { email, password, name } = req.body;
+    if (!email || !password || !name) {
+        return res.status(400).json('Incorrect form submission');
+    }
+    const hash = bcrypt.hashSync(password);
+    database.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+                .returning('*')
+                .insert({
+                    email: loginEmail[0],
+                    name: name,
+                    joined: new Date()
+                })
+                .then(user => {
+                    res.json(user[0]);
+                })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback);
+    })
+    .catch(err => res.status(400).json('Unable to register'));
+});
 
-app.listen(process.env.PORT || 3000, ()=> {
-    console.log(`We are alive on ${process.env.PORT}`);
-})
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    supabase
+      .from('users')
+      .eq('id', id)
+      .increment('entries', 1)
+      .then(response => {
+          if (response.error) {
+              return res.status(400).json('Unable to update entries');
+          }
+          return res.json(response.data[0]);
+      })
+      .catch(err => res.status(400).json('Unable to update entries'));
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`App is running on port ${process.env.PORT}`);
+});
+
+// Netlify Functions syntax for a serverless backend
+module.exports.handler = serverless(app);
